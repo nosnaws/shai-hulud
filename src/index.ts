09c0e3aca47c5651e7cb79fdf2bc11a4af8f81c3
aswanson-nr <aswanson@newrelic.com>
2022-02-15 13:00:27 -0800

    import newrelic

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
-import express, { Request, Response } from "express"
+require("newrelic");
+import express, { Request, Response } from "express";
 
 import { info, start, move, end } from "./logic";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send(info())
+  res.send(info());
 });
 
 app.post("/start", (req: Request, res: Response) => {
-    res.send(start(req.body))
+  res.send(start(req.body));
 });
 
 app.post("/move", (req: Request, res: Response) => {
-    res.send(move(req.body))
+  res.send(move(req.body));
 });
 
 app.post("/end", (req: Request, res: Response) => {
-    res.send(end(req.body))
+  res.send(end(req.body));
 });
 
 // Start the Express server
 app.listen(port, () => {
-    console.log(`Starting Battlesnake Server at http://0.0.0.0:${port}...`)
+  console.log(`Starting Battlesnake Server at http://0.0.0.0:${port}...`);
 });
