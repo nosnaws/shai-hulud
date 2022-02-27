771c84191d7208f69fc9ff968ed354975451fc97
aswanson-nr <aswanson@newrelic.com>
2022-02-26 19:23:40 -0800

    get closest snake when comparing moves

--- test/utils/board.test.ts
+++ test/utils/board.test.ts
@@ -1,7 +1,36 @@
-import { createGrid, BFS, getNeighbors, getMoves } from "../../src/utils/board";
+import {
+  createGrid,
+  BFS,
+  getNeighbors,
+  getMoves,
+  hasDuplicates,
+  getClosestSnake,
+} from "../../src/utils/board";
 import { createBoard, createSnake } from "../utils";
 
 describe("utils/board", () => {
+  describe("hasDuplicates", () => {
+    it("detects duplicates", () => {
+      const snake = [
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 0, y: 2 },
+      ];
+      expect(hasDuplicates(snake)).toBe(true);
+    });
+    it("detects no duplicates", () => {
+      const snake = [
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 0, y: 3 },
+      ];
+      expect(hasDuplicates(snake)).toBe(false);
+    });
+    it("handles array with one element", () => {
+      const snake = [{ x: 0, y: 1 }];
+      expect(hasDuplicates(snake)).toBe(false);
+    });
+  });
   describe("createBoard", () => {
     it("generates 0x0 grid", () => {
       const grid = createGrid(createBoard(0));
@@ -144,6 +173,96 @@ describe("utils/board", () => {
       expect(neighbors).toHaveLength(2);
       expect(neighbors[0].coord).toEqual({ x: 6, y: 1 });
       expect(neighbors[1].coord).toEqual({ x: 4, y: 1 });
+    });
+  });
+  describe("getClosestSnake", () => {
+    it("returns snake", () => {
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ r _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ s s s _ _ _ _ _
+      // _ _ _ s _ h _ _ _ _ _
+      // _ _ _ s s s _ _ _ _ _
+
+      const snake = createSnake([
+        { x: 5, y: 1 },
+        { x: 5, y: 0 },
+        { x: 4, y: 0 },
+        { x: 3, y: 0 },
+        { x: 3, y: 1 },
+        { x: 3, y: 2 },
+        { x: 4, y: 2 },
+        { x: 5, y: 2 },
+      ]);
+
+      const grid = createGrid(createBoard(11, [], [snake]));
+      const cSnake = getClosestSnake(grid, { x: 8, y: 4 }, [snake]);
+      expect(cSnake).toBeDefined();
+      expect(cSnake?.head).toEqual({ x: 5, y: 1 });
+    });
+
+    it("returns closest snake", () => {
+      // k _ _ _ _ _ _ _ _ _ _
+      // s _ _ _ _ _ _ _ _ _ _
+      // s _ _ _ _ _ _ _ _ _ _
+      // s _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ _ _ _ _ _ r _ _
+      // _ _ _ _ _ _ _ _ _ _ _
+      // _ _ _ s s s _ _ _ _ _
+      // _ _ _ s _ h _ _ _ _ _
+      // _ _ _ s s s _ _ _ _ _
+
+      const snake = createSnake([
+        { x: 5, y: 1 },
+        { x: 5, y: 0 },
+        { x: 4, y: 0 },
+        { x: 3, y: 0 },
+        { x: 3, y: 1 },
+        { x: 3, y: 2 },
+        { x: 4, y: 2 },
+        { x: 5, y: 2 },
+      ]);
+
+      const snake2 = createSnake([
+        { x: 0, y: 10 },
+        { x: 0, y: 9 },
+        { x: 0, y: 8 },
+        { x: 0, y: 7 },
+      ]);
+      const grid = createGrid(createBoard(11, [], [snake, snake2]));
+      const cSnake = getClosestSnake(grid, { x: 8, y: 4 }, [snake, snake2]);
+      expect(cSnake).toBeDefined();
+      expect(cSnake?.head).toEqual({ x: 5, y: 1 });
+    });
+
+    it("handles no snakes", () => {
+      const snake = createSnake([
+        { x: 5, y: 1 },
+        { x: 5, y: 0 },
+        { x: 4, y: 0 },
+        { x: 3, y: 0 },
+        { x: 3, y: 1 },
+        { x: 3, y: 2 },
+        { x: 4, y: 2 },
+        { x: 5, y: 2 },
+      ]);
+
+      const snake2 = createSnake([
+        { x: 0, y: 10 },
+        { x: 0, y: 9 },
+        { x: 0, y: 8 },
+        { x: 0, y: 7 },
+      ]);
+      const grid = createGrid(createBoard(11, [], []));
+      const cSnake = getClosestSnake(grid, { x: 8, y: 4 }, [snake, snake2]);
+      expect(cSnake).not.toBeDefined();
     });
   });
 });
